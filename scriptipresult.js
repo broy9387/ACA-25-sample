6d4bce9f1a804a935d45a5c2f2670bbbfa616f17
broy9387 <57662910+broy9387@users.noreply.github.com>
2021-02-04 17:52:06 +0530

    Add files via upload

---
+++ scriptipresult.js
@@ -0,0 +1,13 @@
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyB2f7wlVI_4zq1NbmJZjFnNCOsT_EQ-IJQ",
+    authDomain: "ipl-results-eff36.firebaseapp.com",
+    projectId: "ipl-results-eff36",
+    storageBucket: "ipl-results-eff36.appspot.com",
+    messagingSenderId: "310620157132",
+    appId: "1:310620157132:web:bf8e941c6d3e9a780dbe69"
+};
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+</script>
